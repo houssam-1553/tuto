977fc18b9755b346ce67b9ef4cefdde34db2915b
Houssam <h.zitoune@esi- sba.dz>
2023-08-03 09:01:14 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,258 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        fontFamily: 'Lalezar-Regular', // Specify your custom font family here
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SplashScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  bool isLogoBig = false;
+  late AnimationController _controller;
+  final Duration animationDuration = Duration(milliseconds: 500);
+  final Duration splashDuration = Duration(seconds: 3);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: animationDuration,
+    );
+
+    // Start the logo animation after a delay
+    Timer(splashDuration, () {
+      toggleLogoSize();
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void toggleLogoSize() {
+    setState(() {
+      isLogoBig = !isLogoBig;
+    });
+
+    if (isLogoBig) {
+      _controller.forward().whenComplete(() {
+        // Navigation to the next page after the animation completes
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => NextPage(
+              gradientColors: [
+                Colors.purple[300]!,
+                Colors.cyan,
+              ],
+            ),
+          ),
+        );
+      });
+    } else {
+      _controller.reverse();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        // Add box decoration
+        decoration: BoxDecoration(
+          // Box decoration takes a gradient
+          gradient: LinearGradient(
+            // Where the linear gradient begins and ends
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            // Add one stop for each color. Stops should increase from 0 to 1
+            colors: [
+              // Colors are easy thanks to Flutter's Colors class.
+              Colors.purple[300]!,
+              Colors.cyan,
+            ],
+          ),
+        ),
+        child: Center(
+          child: GestureDetector(
+            onTap: toggleLogoSize,
+            child: AnimatedContainer(
+              duration: animationDuration,
+              width: isLogoBig ? 400 : 000, // Set the desired width of the logo
+              height: isLogoBig ? 400 : 000, // Set the desired height of the logo
+              child: LogoWidget(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class LogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Replace this with your logo image widget, for example, you can use Image.asset or Image.network
+    return Image.asset(
+      'assets/images/logo.png', // Replace 'assets/logo.png' with the path to your logo image
+    );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  final List<Color> gradientColors;
+
+  NextPage({required this.gradientColors});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: gradientColors,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Implement button functionality
+                  // For example, you can navigate to another page or perform some action.
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white70, // Change the button color
+                  foregroundColor: Colors.white, // Change the text color
+                  maximumSize: Size (200,120),
+
+                  padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10), // Adjust button padding
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10), // Round button corners
+                  ),
+                ),
+                child: Column(
+
+                  mainAxisSize: MainAxisSize.min,
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: [
+                    Text('3 Players Mode',
+                      style: TextStyle( fontSize: 20),),
+                    Image.asset('assets/images/arbitre.png',),
+
+                    Center(
+
+
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        
+                        children: [
+                        Image.asset(
+                          'assets/images/user1.png',
+                        ),
+                          SizedBox(
+                            width: 20, //<-- SEE HERE
+                          ),
+                         Text('VS'),
+                          SizedBox(
+                            width: 20, //<-- SEE HERE
+                          ),
+                       Image.asset(
+                            'assets/images/user2.png',
+                       ),
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement button functionality
+                  // For example, you can navigate to another page or perform some action.
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.lime, // Change the button color
+                  foregroundColor: Colors.white, // Change the text color
+                  maximumSize: Size (200,120),
+
+                  padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10), // Adjust button padding
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10), // Round button corners
+                  ),
+                ),
+                child: Column(
+
+                  mainAxisSize: MainAxisSize.min,
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: [
+                    Text('5 Players Mode',
+                      style: TextStyle( fontSize: 20),),
+                    Image.asset('assets/images/arbitre.png',),
+
+                    Center(
+
+
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+
+                        children: [
+                          Image.asset(
+                            'assets/images/user1.png',
+                          ),Image.asset(
+                            'assets/images/user1.png',
+                          ),
+                          SizedBox(
+                            width: 20, //<-- SEE HERE
+                          ),
+                          Text('VS'),
+                          SizedBox(
+                            width: 20, //<-- SEE HERE
+                          ),
+                          Image.asset(
+                            'assets/images/user2.png',
+                          ),Image.asset(
+                            'assets/images/user2.png',
+                          ),
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
